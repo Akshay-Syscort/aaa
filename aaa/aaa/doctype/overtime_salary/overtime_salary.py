dac3ea929fc4d5082968ec3dad4f4a83e12afbac
Akshay-Syscort <akshay.shaharwale@syscort.com>
2025-02-24 14:39:10 +0530

    Overtime Salary calculation update

--- aaa/aaa/doctype/overtime_salary/overtime_salary.py
+++ aaa/aaa/doctype/overtime_salary/overtime_salary.py
@@ -7,10 +7,21 @@ from frappe.model.document import Document
 
 
 class overtimesalary(Document):
-    def before_save(self):
+    
+    
+    # def validate(self):
+    #     total_working_days=float(self.total_working_days)
+    #     absent_days=float(self.absent_days)
+        
+        
+    #     temp_total_present_days = total_working_days - absent_days
+    #     frappe.msgprint(temp_total_present_days)
+        
+        
+    def validate(self):
         employee_name=self.employee_name
-        normal_ot=self.normal_ot
-        holiday_ot=self.holiday_ot
+       
+        
     
         
         
@@ -25,20 +36,103 @@ class overtimesalary(Document):
         if latest_entry:
             latest_doc = latest_entry[0]
             self.base_salary=latest_doc["base"]
+            # print(self.base_salary)
         
-        self.total_present_days = self.total_working_days - self.absent_days
         
-        #Overtime Calculation
-        int_base_salary =int(self.base_salary)
-        temp= (int_base_salary/30)/8
-        var=int(temp)
         
-        temp_normal_overtime =(var * 1.2)* int(normal_ot)
-        # print(temp_normal_overtime)
-        self.normal_overtime_amount=temp_normal_overtime
+    def before_save(self):
+            print(self.total_working_days)
+            print(self.absent_days)
+            float_total_working_days=float(self.total_working_days)
+            float_absent_days=float(self.absent_days)
+            print(float_total_working_days)
+            print(float_absent_days)
+
+            
+            
+          
+            
+            
+            # Attendance Calculation
+            float_present_day=float_total_working_days - float_absent_days
+            print(float_present_day)
+            self.total_present_days=float_present_day
+            
+            #Normal Overtime Calculation
+            normal_ot=float(self.normal_ot)  
+            temp_normal_overtime=(float(self.base_salary)/30/8)
+            #mul 1.2
+            temp_normal_overtime1=temp_normal_overtime*1.2
+            #mul* normal ot
+            temp_normal_overtime2=temp_normal_overtime1* normal_ot
+            self.normal_overtime_amount=temp_normal_overtime2
+          
+            
+      
+            #holiday Overtime
+            holiday_ot=float(self.holiday_ot)
+            # mul *1.5
+            temp_normal_overtime3=temp_normal_overtime*1.5
+            temp_normal_overtime4=temp_normal_overtime3 * holiday_ot
+            self.holiday_overtime_amount=temp_normal_overtime4
+            
+            
+            
+          
+            
+            
+            #Holiday Overtime
+            # holiday_ot=float(self.holiday_ot)
+            # print(holiday_ot)
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        # Calculate present_days
+        # present_days = self.total_working_days - self.absent_days
+        # self.total_present_days=present_days
         
-        temp_holiday_overtime=(var * 1.2)* int(holiday_ot)
-        self.holiday_overtime_amount=temp_holiday_overtime
+        
+        # if latest_entry:
+        #     latest_doc = latest_entry[0]
+        #     self.base_salary=latest_doc["base"]
+        #     frappe.msgprint(latest_doc)
+            
+        #     self.base_Salary=temp_base_salary
+            
+        # frappe.msgprint(total_working_days)
+        # frappe.msgprint(absent_days)
+
+        
+        # total_present_days = self.total_working_days - self.absent_days
+        # frappe.msgprint(total_present_days)
+        
+        
+        
+        # #Overtime Calculation
+    
+        
+        # temp_value=float_base_salary/30/8
+        # frappe.msgprint(temp_value)
+        
+        # frappe.msgprint(temp1)
+        # temp= (int_base_salary/30)/8
+        # var=float(temp)
+        
+        # temp_normal_overtime =(var * 1.2)* float(normal_ot)
+        # # print(temp_normal_overtime)
+        # self.normal_overtime_amount=temp_normal_overtime
+        
+        # temp_holiday_overtime=(var * 1.2)* float(holiday_ot)
+        # self.holiday_overtime_amount=temp_holiday_overtime
+    
+    
+        
         
     def before_submit(self):
         additional_salary_normal_overtime= frappe.get_doc({
